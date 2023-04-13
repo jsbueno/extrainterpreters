8df266957ee50fcedaa3a95f78c0400aad2294df
Joao S O Bueno <gwidion@gmail.com>
2023-04-13 15:30:02 -0300

    Change 'run'calls to take arguments seamleslly. Add contextmanager interface

--- extrainterpreters/extrainterpreters.py
+++ extrainterpreters/extrainterpreters.py
@@ -24,15 +24,55 @@ BFSZ = 10_000_000
 RET_OFFSET = 8_000_000
 
 class Interpreter:
+    """High level Interpreter object
+
+    Simply instantiate this, and use as a context manager
+    (or call `.start()` to make calls that will execute in the
+    subinterpreter.
+
+    If the `run_in_thread` call is used, a new thread is created
+    and the sub-interpreter will execute code in that thread.
+
+    Pickle is used to translate functions to the subinterpreter - so
+    only pick-able callables can be used.
+    """
+
+    # TBD: add async interface
+
     def __init__(self):
-        self.intno = interpreters.create()
-        self.fname = tempfile.mktemp()
-        self.file = open(self.fname, "w+b")
-        self.file.write(b"\x00" * BFSZ)
-        self.fileno = self.file.fileno()
-        self.map = mmap.mmap(self.fileno, BFSZ)
+        self.intno = None
+        self.lock = threading.Lock()
+
+    def start(self):
+        with self.lock:
+            self.intno = interpreters.create()
+            self.fname = tempfile.mktemp()
+            self.file = open(self.fname, "w+b")
+            self.file.write(b"\x00" * BFSZ)
+            self.fileno = self.file.fileno()
+            self.map = mmap.mmap(self.fileno, BFSZ)
+            self.thread = None
+            self._init_interpreter()
+            return self
+
+    __enter__ = start
+
+    def close(self, *args):
+        with self.lock:
+            interpreters.destroy(self.intno)
+            try:
+                self.map.close()
+                self.file.close()
+            finally:
+                os.unlink(self.fname)
+            self.map = None
+            self.intno = False
         self.thread = None
-        self._init_interpreter()
+        # TBD: check if there was a task running in a thread,
+        # if so, and it is done, retrieve the result for further
+        # use.
+
+    __exit__ = close
 
     def _init_interpreter(self):
         code = D(f"""\
@@ -61,7 +101,37 @@ class Interpreter:
         """)
         interpreters.run_string(self.intno, code)
 
-    def run(self, func, args=(), kwargs=None):
+    def run(self, func, *args, **kwargs):
+        """Call to execute a function in the sub-interpreter synchronously"""
+        self.execute(func, args=args, kwargs=kwargs)
+        # Answer should be synchronous
+        return self.result()
+
+    def run_in_thread(self, func, *args, **kwargs):
+        """Call to execute a function in a different thread.
+
+        Call `.done()` to check the execution is complete
+        and `.result()` to retrieve the return value.
+        """
+        self.thread = threading.Thread(target=self.execute, args=(func,),  kwargs={"args": args, "kwargs":kwargs})
+        self.thread.start()
+        return True
+
+    def execute(self, func, args=(), kwargs=None):
+        """Lower level function to actual dispatch the call
+        to the subinterpreter in the current running thread.
+
+        Prefer to use `.run` to run in the same thread
+        or `.run_in_thread` for now.
+        """
+
+        # TBD: allow multi-threaded parallel calls (in parent intertpreter)
+        # to sub-interpreter.
+
+        if self.intno is None:
+            raise RuntimeError(D("""\
+                Sub-interpreter not initialized. Call ".start()" or enter context to make calls.
+            """))
         self.map[RET_OFFSET] == 0
         kwargs = kwargs or {}
         self.map.seek(0)
@@ -75,10 +145,6 @@ class Interpreter:
             self.map[RET_OFFSET] = True
             self.exception = error
             raise
-
-    def run_in_thread(self, func, args=(), kwargs=None):
-        self.thread = threading.Thread(target=self.run, args=(func,),  kwargs={"args": args, "kwargs":kwargs})
-        self.thread.start()
 
     def done(self):
         # TBD: check interpreters.is_running?
@@ -94,14 +160,5 @@ class Interpreter:
             self.thread = None
         return result
 
-    def __del__(self):
-        self.thread = None
-        interpreters.destroy(self.intno)
-        try:
-            self.map.close()
-            self.file.close()
-        finally:
-            os.unlink(self.fname)
-
     def __repr__(self):
-        return f"Sub-Interpreter <n. {self.intno}> "
+        return f"Sub-Interpreter <#. {self.intno}>"
