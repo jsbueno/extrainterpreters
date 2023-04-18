af0c126491ba8b947788e4da0a91fbca6625418a
Joao S O Bueno <gwidion@gmail.com>
2023-04-17 21:19:57 -0300

    Refactor mmaped buffer

--- extrainterpreters/extrainterpreters.py
+++ extrainterpreters/extrainterpreters.py
@@ -104,7 +104,6 @@ class BaseInterpreter:
             raise RuntimeError("Interpreter already started")
         with self.lock:
             self.intno = interpreters.create()
-            self.fname = tempfile.mktemp()
             running_interpreters.add(self)
             self.thread = None
             self._create_channel()
@@ -136,36 +135,11 @@ class BaseInterpreter:
         pass
 
     def _init_interpreter(self):
-        code = D(f"""\
-            import mmap
-            import pickle
-            import sys
-            sys.path[:] = {sys.path}
-
-            BFSZ = {BFSZ}
-            RET_OFFSET = {RET_OFFSET}
-            _m = mmap.mmap({self.fileno}, BFSZ)
-
-            def _thaw(ind_data):
-                _m.seek(ind_data)
-                func = pickle.load(_m)
-                args = pickle.load(_m)
-                kw = pickle.load(_m)
-                return func, args, kw
-
-            def _call(ind_data):
-                func, args, kw = _thaw(ind_data)
-                result = func(*args, **kw)
-                _m.seek(RET_OFFSET)
-                pickle.dump(result, _m)
-
-            def _exit():
-                global _m
-                _m.close()
-                del _m
-
-        """)
+        code = self._interp_init_code()
         interpreters.run_string(self.intno, code)
+
+    def _interp_init_code(self):
+        return ""
 
     def run(self, func, *args, **kwargs):
         """Call to execute a function in the sub-interpreter synchronously"""
@@ -338,24 +312,49 @@ class BaseInterpreter:
 class MMapInterpreter(BaseInterpreter):
 
     def _create_channel(self):
-        self.fname = tempfile.mktemp()
-        self.file = open(self.fname, "w+b")
-        self.file.write(b"\x00" * BFSZ)
-        self.file.flush()
-        self.fileno = self.file.fileno()
-        self.map = mmap.mmap(self.fileno, BFSZ)
+        self.buffer = ProcessBuffer()
+        self.map = self.buffer.map
         super()._create_channel()
 
     def _close_channel(self):
         with self.lock:
-            try:
-                self.map.close()
-                self.file.close()
-            finally:
-                os.unlink(self.fname)
+            self.buffer.__del__()
             self.map = None
             self.intno = False
         super()._close_channel()
+
+    def _interp_init_code(self):
+        code = super()._interp_init_code()
+        code += D(f"""\
+            import mmap
+            import pickle
+            import sys
+            sys.path[:] = {sys.path}
+
+            BFSZ = {BFSZ}
+            RET_OFFSET = {RET_OFFSET}
+            _m = mmap.mmap({self.buffer.fileno}, BFSZ)
+
+            def _thaw(ind_data):
+                _m.seek(ind_data)
+                func = pickle.load(_m)
+                args = pickle.load(_m)
+                kw = pickle.load(_m)
+                return func, args, kw
+
+            def _call(ind_data):
+                func, args, kw = _thaw(ind_data)
+                result = func(*args, **kw)
+                _m.seek(RET_OFFSET)
+                pickle.dump(result, _m)
+
+            def _exit():
+                global _m
+                _m.close()
+                del _m
+
+        """)
+        return code
 
 
 class WorkerOpcodes: # WorkerOpecodes
@@ -410,20 +409,57 @@ class Field:
 
 
 class StructBase:
+    """A Struct type class which can attach to offsets in an existing memory buffer
+
+    Currently, only different sized integer fields are implemented.
+    """
+
     slots = ("_data", "_offset")
     def __init__(self, _data, _offset):
         self._data = _data
         self._offset = _offset
 
     @clsproperty
-    def size(cls):
+    def _size(cls):
         size = 0
         for name, obj in cls.__dict__.items():
             if isinstance(obj, Field):
                 size += obj.size
         return size
 
+    def _detach(self):
+        self._data = self._data[self._offset: self._offset + self._size]
+        self._offset = 0
 
+
+class ProcessBuffer:
+    def __init__(self):
+        self.fname = tempfile.mktemp()
+        self.file = open(self.fname, "w+b")
+        self.file.write(b"\x00" * BFSZ)
+        self.file.flush()
+        self.fileno = self.file.fileno()
+        self.map = mmap.mmap(self.fileno, BFSZ)
+
+    def __del__(self):
+        # Should be called explicitly by users of the class
+        try:
+            if self.map:
+                self.map.close()
+            if self.file:
+                self.file.close()
+        finally:
+            try:
+                os.unlink(self.fname)
+            except FileNotFoundError:
+                pass
+        self.map = self.file = None
+
+    def __repr__(self):
+        return f"<Mmaped interprocess buffer with {BFSZ:_} bytes>"
+
+
+""" #WIP
 class PipedInterpreter(BaseInterpreter):
 
     def _create_channel(self):
@@ -435,166 +471,7 @@ class PipedInterpreter(BaseInterpreter):
             os.close(self.pipe.reader)
             os.close(self.pipe.writer)
         super().close()
-
-
-    def _init_interpreter(self):
-        code = D(f"""\
-            import mmap
-            import pickle
-            import sys
-            sys.path[:] = {sys.path}
-
-            BFSZ = {BFSZ}
-            RET_OFFSET = {RET_OFFSET}
-            _m = mmap.mmap({self.fileno}, BFSZ)
-
-            def _thaw(ind_data):
-                _m.seek(ind_data)
-                func = pickle.load(_m)
-                args = pickle.load(_m)
-                kw = pickle.load(_m)
-                return func, args, kw
-
-            def _call(ind_data):
-                func, args, kw = _thaw(ind_data)
-                result = func(*args, **kw)
-                _m.seek(RET_OFFSET)
-                pickle.dump(result, _m)
-
-            def _exit():
-                global _m
-                _m.close()
-                del _m
-
-        """)
-        interpreters.run_string(self.intno, code)
-
-    def run(self, func, *args, **kwargs):
-        """Call to execute a function in the sub-interpreter synchronously"""
-        self.execute(func, args=args, kwargs=kwargs)
-        # Answer should be synchronous
-        return self.result()
-
-    def run_in_thread(self, func, *args, **kwargs):
-        """Call to execute a function in a different thread.
-
-        Call `.done()` to check the execution is complete
-        and `.result()` to retrieve the return value.
-        """
-        self.thread = threading.Thread(target=self.execute, args=(func,),  kwargs={"args": args, "kwargs":kwargs})
-        self.thread.start()
-        return True
-
-    def _source_handle(self, func):
-
-        if func.__name__ in self._source_handled and hash(func) == self._source_handled[func.__name__]:
-            return
-        source = inspect.getsource(func)
-        self.run_string(source)  # "types" function in remote interpreter __main__
-        self._source_handled[func.__name__] = hash(func)
-
-    def _handle_module(self, mod_name):
-        if mod_name and not mod_name in self._source_handled:
-            try:
-                self.run_string(f"import {mod_name}")
-                self._source_handled[mod_name] = "module"
-            except interpreters.RunFailedError:
-                # certain modules won't load in subinterpreters.
-                # if the called function would need them, it would
-                # not work all the same.
-                pass
-
-    def _prepare_interactive(self, func):
-        # rebuilds part of the environment of the interactive interpreter
-        # in the sub-interpreter - including a interactively typed function
-        # thre are limits for what can reasonably be done here:
-        # we will inspect the globals in the interactive shell
-        # and import all root-level modules in the sub-interpreter
-        # but not sub-modules. e.g.: if the user imported "math"
-        # we will import that into the sub interpreter.
-        # if the user imported "xml.etree", just
-        # "xml" will be imported, and if the user tries
-        # to use "xml.etree" inside the interactive
-        # function, that will fail.
-
-        assert func.__module__ == "__main__"
-
-        if not hasattr(self, "_source_handled"):
-            self._source_handled = {}
-
-        self._source_handle(func)
-
-        main_module = sys.modules["__main__"]
-        main_globals = main_module.__dict__
-
-        for name, obj in list(main_globals.items()):
-            if isinstance(obj, FunctionType):
-                if (mod_name:=getattr(obj, "__module__", None)) == "__main__":
-                    self._source_handle(obj)
-                else:
-                    self._handle_module(mod_name)
-                    # Poor man's "from x import y"
-                    if func.__name__ not in self._source_handled:
-                        self.run_string(f"{name} = getattr({mod_name}, '{obj.__name__}')")
-            elif isinstance(obj, ModuleType):
-                mod_name = obj.__name__
-                self._handle_module(mod_name)
-                if mod_name != name and name not in self._source_handled:
-                    self.run_string(f"{name} = {mod_name}")
-            # Test: is it worth setting literal objects that might be
-            # used as global variables?
-
-    def execute(self, func, args=(), kwargs=None):
-        """Lower level function to actual dispatch the call
-        to the subinterpreter in the current running thread.
-
-        Prefer to use `.run` to run in the same thread
-        or `.run_in_thread` for now.
-        """
-
-        # TBD: allow multi-threaded parallel calls (in parent intertpreter)
-        # to sub-interpreter.
-        if self.intno is None:
-            raise RuntimeError(D("""\
-                Sub-interpreter not initialized. Call ".start()" or enter context to make calls.
-                """))
-
-        revert_main_name = False
-        if getattr(func, "__module__", None) == "__main__":
-            if (mod_name:=getattr(mod_name:=sys.modules["__main__"], "__file__", None)):
-                revert_main_name = True
-                mod = __import__(Path(mod_name).stem)
-                func = getattr(mod, func.__name__)
-            else:
-                self._prepare_interactive(func)
-
-        self.map[RET_OFFSET] == 0
-        kwargs = kwargs or {}
-        self.map.seek(0)
-        _failed = False
-        for obj in (func, args, kwargs):
-            try:
-                pickle.dump(obj, self.map)
-            except ValueError:
-                _failed = True
-            if _failed or self.map.tell() >= BFSZ - RET_OFFSET:
-                raise RuntimeError(D(f"""\
-                    Payload to subinterpreter larger than payload buffer.
-                    Call cancelled. If needed, just make buffer larger by tweaking
-                    extrainterpreters' {BFSZ=} and {RET_OFFSET=} values.
-                    """))
-
-        if revert_main_name:
-            mod.__name__ = "__main__"
-
-        code = "_call(0)"
-        try:
-            interpreters.run_string(self.intno, code)
-        except interpreters.RunFailedError as error:
-            self.map[RET_OFFSET] = True
-            self.exception = error
-            print(f"Failing code\n {func}(*{args}, **{kwargs})\n, passed as {code}", file=sys.stderr)
-            raise
+"""
 
 Interpreter = MMapInterpreter
 
@@ -618,7 +495,7 @@ def destroy_dangling_interpreters():
             import warnings
             warnings.warn(D(f"""\
                 Failed to close dangling {interp}. Data exchange file
-                {interp.fname} may not have been erased.
+                {interp.buffer.fname} may not have been erased.
                 """))
         else:
             print(f"{interp} closed at main interpreter exit", file=sys.stderr)
--- tests/test_struct.py
+++ tests/test_struct.py
@@ -8,7 +8,7 @@ def test_struct_works():
         data_offset = Field(2)
         length = Field(4)
 
-    assert Struct.size == 2 + 4
+    assert Struct._size == 2 + 4
     s = Struct(bytearray(b"\x00" * 6 ), 0)
     s.data_offset = 1000
     s.length = 2_000_000
@@ -22,11 +22,28 @@ def test_struct_with_offset():
         data_offset = Field(2)
         length = Field(4)
 
-    assert Struct.size == 2 + 4
+    assert Struct._size == 2 + 4
     s = Struct(bytearray(b"\x00" * 16 ), 10)
     s.data_offset = 1000
     s.length = 2_000_000
 
+    assert s.data_offset == 1000
+    assert s.length == 2_000_000
+
+
+def test_struct_detach():
+    class Struct(StructBase):
+        data_offset = Field(2)
+        length = Field(4)
+
+    s = Struct(data:=bytearray(b"\x00" * 16 ), 10)
+    s.data_offset = 1000
+    s.length = 2_000_000
+
+    assert data is s._data
+    s._detach()
+    assert data is not s._data
+    assert s._offset == 0
     assert s.data_offset == 1000
     assert s.length == 2_000_000
 
